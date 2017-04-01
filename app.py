bbfd7fa14e9e443cdb94caf22e7fd22b7cc66777
Erik Yang <erikyang@Chisius-MacBook-Air.local>
2017-04-01 13:24:02 -0700

    Basic get method

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 #!jobqueuevirtualenv/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return "Hello World!"
+queue_tasks = [
+	{
+		'id': 1,
+		'url': 'http://erikyangs.github.io/',
+		'status': 'Done'
+	},
+	{
+		'id': 2,
+		'url': 'http://petr-lee.github.io/',
+		'status': 'In Progress'
+	},
+	{
+		'id': 3,
+		'url': 'http://tianruichen.github.io/',
+		'status': 'Waiting'
+	}
+]
+
+@app.route('/jobqueue/api/v1.0/queue_tasks', methods=['GET'])
+def getqueue():
+	return jsonify({'queue_tasks': queue_tasks})
 
 if __name__ == '__main__':
 	app.run(debug=True)
