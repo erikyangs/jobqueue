c5663b258a80817b5978d503ce128c0a9a4309cf
Erik Yang <erikyang@Chisius-MacBook-Air.local>
2017-04-02 04:14:53 -0700

    REST API and fetching data work - some bugs left to fix

--- app.py
+++ app.py
@@ -1,11 +1,63 @@
 #!jobqueuevirtualenv/bin/python
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, render_template
+import urllib2
+
+# ===================================
+
+from Queue import Queue
+from threading import Thread
+
+q = Queue()
+
+def worker():
+    print "Started worker thread"
+    while True:
+        task = q.get()
+        task['status'] = 'In Progress'
+        do_work(task)
+        task['status'] = 'Done'
+        q.task_done()
+
+def do_work(task):
+	task_url = task['url']
+	task_HTML = getURLHTML(task_url)
+	result = {
+		'id': task['id'],
+		'data': task_HTML
+	}
+	results.append(result)
+
+for i in range(2):
+    t = Thread(target=worker)
+    t.daemon = True
+    t.start()
+
+def getURLHTML(url):
+	return urllib2.urlopen(url).read()
+
+# ===================================
 
 app = Flask(__name__)
 
-queue_tasks = [
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-]
+queue_tasks = []
+results = []
+
+#get list of results
+@app.route('/jobqueue/api/v1.0/results', methods=['GET'])
+def getresults():
+	return jsonify({'results': results})
+
+#get specific result
+@app.route('/jobqueue/api/v1.0/results/<int:result_id>', methods=['GET'])
+def get_result(result_id):
+    result = [result for result in results if result['id'] == result_id]
+    if len(result) == 0:
+        abort(404)
+    return jsonify({'result': result[0]})
 
 #get list of queue tasks
 @app.route('/jobqueue/api/v1.0/queue_tasks', methods=['GET'])
@@ -38,6 +90,7 @@ def create_task():
         'status': 'Waiting'
     }
     queue_tasks.append(task)
+    q.put(task)
     return jsonify({'queue_task': task}), 201
 
 if __name__ == '__main__':
