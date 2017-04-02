bcfb1819c46329d814fae256295cce112485df85
Erik Yang <erikyang@Chisius-MacBook-Air.local>
2017-04-01 17:37:12 -0700

    REST API relevant methods finished

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
 #!jobqueuevirtualenv/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__)
 
 queue_tasks = [
-	{
-		'id': 1,
-		'url': 'http://erikyangs.github.io/',
-		'status': 'Done'
-	},
-	{
-		'id': 2,
-		'url': 'http://petr-lee.github.io/',
-		'status': 'In Progress'
-	},
-	{
-		'id': 3,
-		'url': 'http://tianruichen.github.io/',
-		'status': 'Waiting'
-	}
+
 ]
 
+#get list of queue tasks
 @app.route('/jobqueue/api/v1.0/queue_tasks', methods=['GET'])
 def getqueue():
 	return jsonify({'queue_tasks': queue_tasks})
+
+#get specific queue task
+@app.route('/jobqueue/api/v1.0/queue_tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in queue_tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'queue_task': task[0]})
+
+#create a new queue task
+@app.route('/jobqueue/api/v1.0/queue_tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'url' in request.json:
+        abort(400)
+
+    #handle empty dictionary
+    if len(queue_tasks)>0:
+    	task_id = queue_tasks[-1]['id'] + 1
+    else:
+    	task_id = 1
+    #task to be posted
+    task = {
+        'id': task_id,
+        'url': request.json['url'],
+        'status': 'Waiting'
+    }
+    queue_tasks.append(task)
+    return jsonify({'queue_task': task}), 201
 
 if __name__ == '__main__':
 	app.run(debug=True)
